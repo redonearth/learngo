7fc3b00db3862cb6676f7eba165eafac53f9d117
redonearth <wona23@gmail.com>
2020-09-27 22:42:59 +0900

    #2.2 Methods part Two

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/redonearth/learngo/accounts"
 )
@@ -9,5 +10,10 @@ import (
 func main() {
 	account := accounts.NewAccount("Redo")
 	account.Deposit(10)
+	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		log.Fatalln(err)
+	}
 	fmt.Println(account.Balance())
 }
