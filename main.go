2056bcbd1a4b8e47e55f4ef2b9df3284f3fe9923
Redonearth <wona23@gmail.com>
2020-12-02 22:44:11 +0900

    #3.0 hitURL

--- main.go
+++ main.go
@@ -1,21 +1,36 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/redonearth/learngo/mydict"
+	"net/http"
 )
 
+var errRequestFailed = errors.New("Request failed")
+
 func main() {
-	dictionary := mydict.Dictionary{"first": "First word"}
-	baseWord := "hello"
-	dictionary.Add(baseWord, "First")
-	dictionary.Search(baseWord)
-	dictionary.Delete(baseWord)
-	word, err := dictionary.Search(baseWord)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(word)
+	urls := []string{
+		"https://www.google.com/",
+		"https://www.github.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://www.airbnb.com/",
+		"https://www.naver.com/",
+		"https://academy.nomadcoders.co/",
 	}
+	for _, url := range urls {
+		hitURL(url)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
 }
