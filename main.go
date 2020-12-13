40f1ab1d610c0ca9ecaf15b53364a1667364b709
Redonearth <wona23@gmail.com>
2020-12-13 21:23:07 +0900

    #3.4 Channels Recap

--- main.go
+++ main.go
@@ -6,17 +6,18 @@ import (
 )
 
 func main() {
-	channel := make(chan bool)
-	people := [2]string{"Redonearth", "Koongdori"}
+	channel := make(chan string)
+	people := [4]string{"Redonearth", "Koongdori", "Hangyeol", "Wineking"}
 	for _, person := range people {
 		go isCool(person, channel)
 	}
-	fmt.Println(<-channel)
-	fmt.Println(<-channel)
+	for i := 0; i < len(people); i++ {
+		fmt.Println(<-channel)
+	}
+
 }
 
-func isCool(person string, channel chan bool) {
-	time.Sleep(time.Second * 3)
-	fmt.Println(person)
-	channel <- true
+func isCool(person string, channel chan string) {
+	time.Sleep(time.Second * 5)
+	channel <- person + " is cool"
 }
